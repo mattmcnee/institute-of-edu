cb51ca7edf1f6d20b8acee178f3642caecaf27e1
Matthew McNee <matthew.mcnee@student.manchester.ac.uk>
2023-10-05 21:00:08 +0100

    started work on login and learn pages

---
+++ learn/learn-video.js
@@ -0,0 +1,37 @@
+// Load the YouTube API script asynchronously
+function loadYouTubeAPI() {
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+// Initialize the YouTube Player API
+function onYouTubeIframeAPIReady() {
+  // Video ID of the YouTube video you want to load
+  var videoId = 'iTRxuBxttj8';
+
+  // Create a new YouTube player instance
+  var player = new YT.Player('player', {
+    width: '100%',
+    height: 'auto',
+    videoId: videoId, // Specify the video ID here
+    playerVars: {
+      start: 22, // Start the video at 22 seconds
+      rel: 0, // Hide related videos
+      showinfo: 0 // Hide video title and uploader information
+    },
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
+
+  // Called when the player is ready
+  function onPlayerReady(event) {
+    // Set the desired playback speed
+    player.setPlaybackRate(1);
+  }
+}
+
+// Load the YouTube API script
+loadYouTubeAPI();
---
+++ learn/learn.js
@@ -0,0 +1,39 @@
+const testAPI_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
+const testVideoId = 'iTRxuBxttj8'; // Replace 'VIDEO_ID' with the ID of the YouTube video you want to fetch details for
+
+// Function to load YouTube video details
+function loadVideoDetails(videoId, API_KEY) {
+  // Fetch video details including snippet and contentDetails
+  fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${API_KEY}&part=snippet,contentDetails`)
+    .then(response => response.json())
+    .then(videoData => {
+      const videoDetails = videoData.items[0].snippet;
+      const title = videoDetails.title;
+      const description = videoDetails.description;
+      const duration = parseDuration(videoData.items[0].contentDetails.duration);
+
+      console.log(description.substring(0, 200));
+
+      // Display video details, description, and subtitles on the webpage
+      const videoDetailsDiv = document.getElementById('video-details');
+      videoDetailsDiv.innerHTML = `
+        <h2>${title}</h2>
+        <p>Description: ${description.substring(0, 200)}</p>
+        <p>Duration: ${duration}</p>
+      `;
+  });
+}
+
+function parseDuration(duration) {
+  const match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+
+  const hours = (match[1] || 0) && parseInt(match[1], 10);
+  const minutes = (match[2] || 0) && parseInt(match[2], 10);
+  const seconds = (match[3] || 0) && parseInt(match[3], 10);
+
+  return `${hours ? hours + 'h ' : ''}${minutes ? minutes + 'm ' : ''}${seconds ? seconds + 's' : ''}`;
+}
+
+loadVideoDetails(testVideoId, testAPI_KEY);
+
+console.log("hello")
